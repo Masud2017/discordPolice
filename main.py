667778ab075de610e9fd76ca3caa97b049607b92
Astra <unroot@localhost.localdomain>
2020-04-04 23:46:48 -0800

    Adding heroku dependencies

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ import re,os,hashlib
 client = discord.Client()
 @client.event
 async def on_message(message):
-    censoredLang = ["snitch","xxx","fuck"]
+    censoredLang = ["snitch","xxx","fuck","chudi"]
     for x in censoredLang:
         if re.search(x,message.content,re.IGNORECASE):
             censored = re.search(x,message.content,re.IGNORECASE)
 
 
-    
     if message.content.find("!hello") != -1:
         await message.channel.send("Hi from bot :D")
     elif message.content.find("!show") != -1:
@@ -20,10 +19,13 @@ async def on_message(message):
         req = requests.get("https://www.googleapis.com/youtube/v3/channels?id=UCyQSAi4Xh5ZQKpMPLEUPSrA&key=AIzaSyBYzmjpQYOb15ueTx3-Y2QcgI8_21Xlhr0&part=snippet,statistics&fields=items(id,snippet,statistics)")
         reqData = json.loads(req.text)
         await message.channel.send(reqData["error"]["errors"][0]["domain"])
+    elif message.content == "!whatIsYourName":
+        await message.channel.send("My name is : Discord-Police\nI am here for save you guys from censored verse :D")
+
     elif message.content == "!whoIsYourMaster":
         await message.channel.send("My Master is : Md.Masud karim\nHe creted me at Saturday 4 April 2020\nTime: 6:16 PM\nHis facebook link is : forbidden.masud")
 
-    elif censored:
+    elif censored: #doing the actuall filtering from here.
         await message.delete()
         await message.channel.send("Gali dewa moha pap.. hawar pola bujhos na ?")
 
@@ -31,4 +33,5 @@ async def on_message(message):
 async def on_member_join(member):
     await channel.send("Welcome new member".str(member))
 
-client.run(PLACE_YOUR_TOKEN)
+#client.run("Njk1OTU2NDYxODUwODUzMzk2.Xohwag.rpUYfcFZNtKRyPdYgoMfZJlwHKU") This token is for Test Server bot
+client.run("NjYwMzQyMDk1OTAyNTM5Nzc3.Xoi2_w.WaP7r29zKZQ_hVwOxzYkGvTI1WQ")
